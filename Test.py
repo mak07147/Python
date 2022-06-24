2b6049279bb86e9fa8249f2a94c104255253a723
mak <mak07147@gmail.com>
2022-06-24 23:44:11 +0300

    Сортировка нечетных чисел в порядке возрастания. Четные числа на первоначальных позициях. (24.06.2022)

--- Test.py
+++ Test.py
@@ -152,6 +152,14 @@
 #     return final_arrs
 # some_array = [2, 22, 5, 1, 4, 11, 37, 0]
 # print(sort_array(some_array))
-
+# Ещё решения оптимальные к задаче выше:
+# ---------------------------------------------------------
+# def sort_array(arr):
+#   odds = sorted((x for x in arr if x%2 != 0), reverse=True)
+#   return [x if x%2==0 else odds.pop() for x in arr]
+# ----------------------------------------------------------
+# def sort_array(source_array):
+#     odds = iter(sorted(v for v in source_array if v % 2))
+#     return [next(odds) if i % 2 else i for i in source_array]
 ################################################################################################################
 
